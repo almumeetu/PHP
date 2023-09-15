034691f34d6e53e338ec963e88dcd493ce9bec87
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2023-09-15 09:01:58 +0600

    practicing PHP

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Document</title>
+</head>
+<body>
+    <?php
+
+    $food = "pizza";
+    $color = "Black";
+    $country = "Bangladesh";
+    echo 'My Name Is Al-Mumeetu Saikat </br>';
+    echo 'I love to eat '.$food;
+    echo '</br> My favorite color is ' .$color;
+    echo '</br> I am form ' .$country;
+    ?>
+</body>
+</html>
